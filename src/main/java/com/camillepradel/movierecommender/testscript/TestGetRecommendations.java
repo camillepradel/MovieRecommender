fff3e00948d116de2793f29c7c37c28b0ed5ad64
Camille31 <camillepradel@gmail.com>
2017-11-11 18:57:01 +0100

    Refine example test script.

--- src/main/java/com/camillepradel/movierecommender/testscript/TestGetRecommendations.java
+++ src/main/java/com/camillepradel/movierecommender/testscript/TestGetRecommendations.java
@@ -9,55 +9,45 @@ import java.net.URL;
 
 public class TestGetRecommendations {
 
-	public static void main(String[] args) {
-		
-		String urlStart = "http://localhost:8080/MovieRecommender/recommendations?user_id=";
-		int nbIterations = 100;
-		int userId= 0;
-		long startTime = System.nanoTime();
+    public static void main(String[] args) {
 
-		for (int i= 0; i < nbIterations; i++) {
-			
-		    URL u;
-		    InputStream is = null;
-		    DataInputStream dis;
+        String urlStart = "http://localhost:8080/MovieRecommender/recommendations?user_id=";
+        int nbIterations = 1000;
+        int userId = 0;
+        long startTime = System.nanoTime();
 
-		    try
-		    {
-		      u = new URL(urlStart + userId);
-		      is = u.openStream();
-		      dis = new DataInputStream(new BufferedInputStream(is));
-		      while ((dis.readLine()) != null)
-		      {
-		      }
-		      System.out.println(i + "/" + nbIterations);
-		    }
-		    catch (MalformedURLException mue)
-		    {
-		      System.err.println("Ouch - a MalformedURLException happened.");
-		      mue.printStackTrace();
-		      System.exit(2);
-		    }
-		    catch (IOException ioe)
-		    {
-		      System.err.println("Oops- an IOException happened.");
-		      ioe.printStackTrace();
-		      System.exit(3);
-		    }
-		    finally
-		    {
-		      try
-		      {
-		        is.close();
-		      }
-		      catch (IOException ioe)
-		      {
-		      }
-		    }
-		}
-		
-		long endTime = System.nanoTime();
-		System.out.println("Time to get " + nbIterations + " times recommendation page: " + (endTime - startTime) + "ns");
+        for (int i = 0; i < nbIterations; i++) {
 
-	}
+            URL u;
+            InputStream is = null;
+            DataInputStream dis;
+
+            try {
+                u = new URL(urlStart + userId);
+                is = u.openStream();
+                dis = new DataInputStream(new BufferedInputStream(is));
+                while (dis.readLine() != null) {
+                }
+                System.out.println(i + "/" + nbIterations);
+            } catch (MalformedURLException mue) {
+                System.err.println("Ouch - a MalformedURLException happened.");
+                mue.printStackTrace();
+                System.exit(2);
+            } catch (IOException ioe) {
+                System.err.println("Oops- an IOException happened.");
+                ioe.printStackTrace();
+                System.exit(3);
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException ioe) {
+                }
+            }
+        }
+
+        long endTime = System.nanoTime();
+        double time = (double) (endTime - startTime) / 1000000000.;
+        System.out.println("Time to process " + nbIterations + " requests in one thread: " + time + "s");
+
+    }
 }
